a854ae3f67ac8c14aa0c2e03bbf2d80158c12a6a
kiseah <k.saturinas.553797@umindanao.edu.ph>
2026-05-23 21:15:56 +0800

    add docker setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,26 +1,27 @@
-<?php
-
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-        Paginator::useBootstrapFive();
-    }
-}
+<?php  
+  
+namespace App\Providers;  
+  
+use Illuminate\Support\ServiceProvider;  
+use Illuminate\Support\Facades\URL;  
+  
+class AppServiceProvider extends ServiceProvider  
+{  
+   /**  
+    * Register any application services.  
+    */  
+   public function register(): void  
+   {  
+       //  
+   }  
+  
+   /**  
+    * Bootstrap any application services.  
+    */  
+   public function boot(): void  
+   {  
+       if (env('APP_ENV') === 'production') {  
+           URL::forceScheme('https');  
+       }  
+   }  
+} 
